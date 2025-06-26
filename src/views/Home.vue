69d5c542daf5040abcc3b9583101b2473ba466b8
k1ngr4m <365717424@qq.com>
2025-06-26 19:55:10 +0800

    feat: queryCurrSummonerInfo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,3 @@
-<template>
-  <div>
-    <div v-if="summonerInfo">
-      <h2>召唤师信息</h2>
-      <pre>{{ summonerInfo }}</pre>
-    </div>
-    <div v-else>
-      <p>加载召唤师信息中...</p>
-    </div>
-  </div>
-</template>
-
 <script setup>
 import { ref, onMounted } from 'vue';
 import request from '../utils/request.js';
@@ -17,7 +5,6 @@ import request from '../utils/request.js';
 const summonerName = ref('');
 const summonerInfo = ref(null);
 
-// 页面加载时自动查询召唤师信息
 onMounted(() => {
   queryCurrSummonerName();
 });
@@ -27,7 +14,7 @@ const querySummonerInfo = async () => {
     const response = await request.post('/v1/summoner/queryByName/info', {
       name: summonerName.value
     });
-    summonerInfo.value = response.data;
+    summonerInfo.value = response.data.data; // 注意这里也要调整
   } catch (error) {
     console.error('请求出错:', error);
     alert('查询召唤师信息失败，请重试');
@@ -37,17 +24,24 @@ const querySummonerInfo = async () => {
 const queryCurrSummonerName = async () => {
   try {
     const response = await request.get('/v1/getCurrSummoner');
-    summonerName.value = response.data.summonerName;
-    summonerInfo.value = response.data;
+    console.log('完整响应:', response); // 调试用
 
-    // 调用查询召唤师详细信息
-    await querySummonerInfo();
+    // 检查响应结构（现在数据在 response.data.data 里）
+    if (response.data && response.data.code === 0 && response.data.data?.summonerName) {
+      summonerName.value = response.data.data.summonerName;
+      console.log('当前召唤师信息:', summonerName.value);
 
-    // 控制台输出以便调试
-    console.log('召唤师信息:', summonerInfo.value);
+      // 如果有召唤师名称，则查询详细信息
+      if (summonerName.value) {
+        await querySummonerInfo();
+      }
+    } else {
+      console.error('响应数据结构不符合预期或请求失败:', response.data);
+      alert('无法获取召唤师信息，请检查客户端状态');
+    }
   } catch (error) {
     console.error('获取召唤师信息失败:', error);
     alert('无法获取召唤师信息，请检查客户端状态');
   }
-}
+};
 </script>
