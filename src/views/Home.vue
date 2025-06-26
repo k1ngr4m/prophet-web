659d476bf68fdeb21819f66568c7ba761c975e0d
k1ngr4m <365717424@qq.com>
2025-06-26 15:02:38 +0800

    feat: queryCurrSummonerInfo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,62 +1,47 @@
 <template>
-    <div>
-      <!-- 当前召唤师信息展示 -->
-      <div v-if="summonerInfo" class="summoner-info">
-        <h2>当前召唤师</h2>
-        <p>{{ summonerInfo.game_name }}#{{ summonerInfo.tag_line }}</p>
-      </div>
-        <input
-                type="text"
-                class="summoner-input"
-                v-model="summonerName"
-                placeholder="输入召唤师名称"
-        />
-        <button @click="queryHorse">查询马匹信息</button>
-        <div v-if="horseInfo">
-            <p>分数: {{ horseInfo.score }}</p>
-            <p>马匹: {{ horseInfo.horse }}</p>
-        </div>
+  <div>
+    <div v-if="summonerInfo">
+      <h2>召唤师信息</h2>
+      <pre>{{ summonerInfo }}</pre>
     </div>
-    <div>
-        <button @click="queryCurrSummonerInfo">查询当前召唤师信息</button>
-        <!-- 显示召唤师信息 -->
-        <div v-if="summonerInfo">
-            <h2>当前召唤师信息</h2>
-            <pre>{{ summonerInfo }}</pre> <!-- 实际使用时建议结构化展示 -->
-        </div>
+    <div v-else>
+      <p>加载召唤师信息中...</p>
     </div>
+  </div>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue';
 import request from '../utils/request.js';
 
-const horseInfo = ref(null);
 const summonerName = ref('');
 const summonerInfo = ref(null);
 
 // 页面加载时自动查询召唤师信息
 onMounted(() => {
-  queryCurrSummonerInfo();
+  queryCurrSummonerName();
 });
 
-const queryHorse = async () => {
+const querySummonerInfo = async () => {
   try {
-    const cleanName = summonerName.value.replace(/[\u2066-\u2069]/g, '');
-    const response = await request.post('/v1/horse/queryBySummonerName', {
-      summonerName: cleanName
+    const response = await request.post('/v1/summoner/queryByName/info', {
+      name: summonerName.value
     });
-    horseInfo.value = response.data;
+    summonerInfo.value = response.data;
   } catch (error) {
     console.error('请求出错:', error);
-    alert('查询马匹信息失败，请重试');
+    alert('查询召唤师信息失败，请重试');
   }
 };
 
-const queryCurrSummonerInfo = async () => {
+const queryCurrSummonerName = async () => {
   try {
     const response = await request.get('/v1/getCurrSummoner');
+    summonerName.value = response.data.summonerName;
     summonerInfo.value = response.data;
+
+    // 调用查询召唤师详细信息
+    await querySummonerInfo();
 
     // 控制台输出以便调试
     console.log('召唤师信息:', summonerInfo.value);
