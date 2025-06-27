7b8bf6e327dac11b5d6a78afac5c4cfc194fafcd
k1ngr4m <365717424@qq.com>
2025-06-27 11:26:01 +0800

    linyiming 进行的更改

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -18,6 +18,7 @@ import request from '../utils/request.js';
 
 const summonerName = ref('');
 const summonerInfo = ref(null);
+const summonerMatchesList = ref([])
 
 onMounted(() => {
   queryCurrSummonerName();
@@ -48,6 +49,7 @@ const queryCurrSummonerName = async () => {
       // 如果有召唤师名称，则查询详细信息
       if (summonerName.value) {
         await querySummonerInfo();
+        await updateSummonerMatches();
       }
     } else {
       console.error('响应数据结构不符合预期:', response);
@@ -57,4 +59,16 @@ const queryCurrSummonerName = async () => {
     alert('无法获取召唤师信息，请检查客户端状态');
   }
 };
+
+const updateSummonerMatches = async () => {
+  try {
+    const response = await request.post('/v1/api/summoner/matches/list', {
+      name: summonerName.value
+    });
+    summonerMatchesList.value = response.data;
+  } catch (error) {
+    console.error('请求出错:', error);
+    alert('查询召唤师历史数据失败，请重试');
+  }
+}
 </script>
